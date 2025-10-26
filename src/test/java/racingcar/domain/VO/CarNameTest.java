8f621d20e76a37b64173f16bcbb6002d43e45102
namucy <wkdcjfdud13@gmail.com>
2025-10-26 14:19:26 +0900

    test(domain): domain의 CarName VO 테스트 코드 작성
    - domain의 CarName VO 테스트 코드를 CarNameTest에 작성합니다.
    - @ParameterizedTest, @CsvSource, @ValueSource 어노테이션을 통해 테스트 코드를 간소화 합니다.

---
+++ src/test/java/racingcar/domain/VO/CarNameTest.java
@@ -0,0 +1,82 @@
+package racingcar.domain.VO;
+
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarNameTest {
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "test, true",
+            "asdf, false"
+
+    })
+    void 동등성_테스트(String input, Boolean expected) {
+        // given
+        CarName original = new CarName("test");
+        CarName target = new CarName(input);
+        // when
+        Boolean isEqual = original.equals(target);
+        // then
+        assertThat(isEqual).isEqualTo(expected);
+    }
+
+
+    @ParameterizedTest
+    @EmptySource
+    void 빈_값_테스트(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> {
+                    CarName name = new CarName(input);
+                })
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void NULL_값_테스트(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> {
+                    CarName name = new CarName(input);
+                })
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 길이_제한_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> {
+                    CarName name = new CarName("asdfas");
+                })
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "우테코",
+            "as1df",
+            "@1a!",
+    })
+    void 영문_외_문자_테스트(String input) {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> {
+                    CarName name = new CarName(input);
+                })
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+}
