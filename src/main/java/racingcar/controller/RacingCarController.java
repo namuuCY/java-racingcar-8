e56f8a41dbb0c0e616cfacff552874f356527668
namucy <wkdcjfdud13@gmail.com>
2025-10-27 18:36:49 +0900

    feat(controller): RacingCarController 구현
    - 입력, 실행, 출력을 관리하는 RacingCarController를 구현합니다.
    - Application에서 의존성을 주입하고, Controller를 실행하는 로직을 작성합니다.

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,40 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.controller.dto.ResponseDto;
+import racingcar.service.RacingCarService;
+import racingcar.utils.InputParser;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingCarService racingCarService;
+
+    public RacingCarController(InputView inputView, OutputView outputView, RacingCarService racingCarService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingCarService = racingCarService;
+    }
+
+    public void run() {
+        List<String> names = scanAndParseNames();
+        Integer trials = scanAndParseTrials();
+
+        ResponseDto response = racingCarService.race(names, trials);
+
+        outputView.read(response, trials);
+    }
+
+    private List<String> scanAndParseNames() {
+        String rawInput = inputView.scanNames();
+        return InputParser.namesParser(rawInput);
+    }
+
+    private Integer scanAndParseTrials() {
+        String trialInput = inputView.scanNames();
+        return InputParser.trialParser(trialInput);
+    }
+}
