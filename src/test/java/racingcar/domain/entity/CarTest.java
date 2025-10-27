89330b11565498fb16b58a25789ee405679d71a1
namucy <wkdcjfdud13@gmail.com>
2025-10-27 11:19:27 +0900

    refactor(test): MockMoveStrategy 분리
    - 이후 테스트에서 재사용 가능성을 염두에 두어, Mock 객체를 생성하는 클래스를 CarTest에서 분리합니다.

--- src/test/java/racingcar/domain/entity/CarTest.java
+++ src/test/java/racingcar/domain/entity/CarTest.java
@@ -9,24 +9,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.VO.Coordinate;
-import racingcar.domain.service.MoveStrategy;
 
 public class CarTest {
 
-    class MockMoveStrategy implements MoveStrategy {
-
-        private final Integer increment;
-
-        public MockMoveStrategy(Integer increment) {
-            this.increment = increment;
-        }
-
-        // 테스트를 위해 random 이 아닌, 직접적으로 증가시킵니다.
-        @Override
-        public Integer moveByStrategy() {
-            return increment;
-        }
-    }
 
     // 생성자
     @ParameterizedTest
